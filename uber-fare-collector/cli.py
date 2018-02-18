a83b91148cc610791e74b065daed08d7b105da19
Kevin Lloyd Bernal <kevinoxy@gmail.com>
2018-02-19 02:10:50 +0800

    improve docs in cli usage

--- uber-fare-collector/cli.py
+++ uber-fare-collector/cli.py
@@ -16,20 +16,25 @@ def validate_coordinate(ctx, param, value):
 
 
 @click.group()
-@click.option('--check-interval', default=120)
-@click.option('--server-token')
+@click.option('--check-interval', default=120, help='Time Interval in seconds '
+        'to check the Uber fares. Default: 120.')
+@click.option('--server-token', help='Server Token to function as API KEY. '
+        'Default: <env var: $UBER_SERVER_TOKEN>')
 @click.pass_context
 def cli(ctx, server_token, check_interval):
     ctx.obj['SERVER_TOKEN'] = server_token or UBER_SERVER_TOKEN
     ctx.obj['CHECK_INTERVAL'] = check_interval
 
 
-@cli.command()
-@click.argument('origin', callback=validate_coordinate)
-@click.argument('destination', callback=validate_coordinate)
-@click.argument('output-file')
+@cli.command(short_help='retrieves the Uber fare estimates.')
+@click.argument('origin', callback=validate_coordinate, metavar='<origin>')
+@click.argument('destination', callback=validate_coordinate, metavar='<dest>')
+@click.argument('output-file', metavar='<output file>')
 @click.pass_obj
 def estimate(ctx, origin, destination, output_file):
+    """This periodically retrieves the Uber fare esimates from <origin>
+    to <dest>."""
+
     fare_estimate(ctx['SERVER_TOKEN'], origin, destination, output_file,
                   ctx['CHECK_INTERVAL'])
 
